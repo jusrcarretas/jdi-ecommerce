13c72b123420e990ad124ca0409d4950eacc34b3
Justin Carretas <jst.carretas@gmail.com>
2019-08-31 15:01:47 +0800

    Update shop/serializers.py - Added Calculated Fields
    - Added average_rating field
    - Added rating_count field
    - Added rating_breakdown field

--- django-jdi/shop/serializers.py
+++ django-jdi/shop/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.db.models import Avg
+from django.db.models import Avg, Count
 from .models import Category, Product, Review
 
 # Create your serializers here.
@@ -14,9 +14,42 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     """Serializer for displaying full product details"""
+    # Declare calculated fields using SerializerMethodField
+    average_rating = serializers.SerializerMethodField()
+    rating_count = serializers.SerializerMethodField()
+    rating_breakdown = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    # Fetch data for the calculated field 'average_rating'
+    def get_average_rating(self, obj):
+        # Get average rating based on reviews attached to the current Product
+        average = obj.product_reviews.aggregate(Avg('rating'))['rating__avg']
+
+        # Return average if there are reviews. Zero if there are none
+        if average:
+            return average
+        else:
+            return 0
+
+    # Fetch data for the calculated field 'rating_count'
+    def get_rating_count(self, obj):
+        # Get review count
+        count = obj.product_reviews.count()
+
+        # Return count
+        return count
+
+    # Fetch data for the calculated field 'rating_count'
+    def get_rating_breakdown(self, obj):
+        # Group ratings by stars then use Count them
+        breakdown = obj.product_reviews.values(
+            'rating').annotate(total=Count('rating')).order_by('-rating')
+
+        # Return breakdown
+        return breakdown
 
 
 class ReviewSerializer(serializers.ModelSerializer):
