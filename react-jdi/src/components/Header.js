1db32805c0060bbc8de31e8da6486f4247f77cbe
Justin Carretas <jst.carretas@gmail.com>
2019-08-31 11:41:48 +0800

    Update src/components/Header.js - Updated Header To Fetch Categories

--- react-jdi/src/components/Header.js
+++ react-jdi/src/components/Header.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './Header.css'
 
 class Header extends Component {
-  state = {  }
+  state = {
+    categories: null
+  }
+
+  // Fetch categories from the backend
+  // Categories will be used to render Navbar Items
+  getCategories = () => {
+    const url = 'http://localhost:8000/api/categories/';
+
+    axios.get(url)
+      .then(response => {
+        console.log(response.data)
+        this.setState({
+          categories: response.data
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
   render() { 
     return (
       <header>
