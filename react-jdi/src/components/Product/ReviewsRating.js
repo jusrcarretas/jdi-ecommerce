6a7cc828c5f72a9b7a07090964e192d79e27b810
Justin Carretas <jst.carretas@gmail.com>
2019-08-31 15:47:36 +0800

    Update src/components/Product/ReviewsRating.js - Added ReviewsRating Component

---
+++ react-jdi/src/components/Product/ReviewsRating.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import { Rate } from 'antd';
+
+const ReviewsRating = props => {
+  const { averageRating } = props;
+
+  return (
+    <Fragment>
+      <div className="inline-block margin-right-10">
+        <div className="rating-details">
+          <div className="text-highlight text-bold text-emphasize4">
+            {averageRating.toFixed(1)}
+          </div>
+          <Rate disabled defaultValue={averageRating} />
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default ReviewsRating;
