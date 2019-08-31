44fdbaba545e26cf29e40b94ddaf2fc7b878e04c
Justin Carretas <jst.carretas@gmail.com>
2019-08-31 16:55:19 +0800

    Update src/components/Product/ReviewsHeader.js - Updated Header Button To Show Modal

--- react-jdi/src/components/Product/ReviewsHeader.js
+++ react-jdi/src/components/Product/ReviewsHeader.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Button } from 'antd';
 
 const ReviewsHeader = props => {
+  const { handleClick } = props;
+
   return (
     <div className="flex">
       <div className="reviews-text flex-1">
         <div>Product Reviews</div>
       </div>
       <div className="write-review flex-1">
-        <Button type="primary">Write a Review</Button>
+        <Button type="primary"
+          onClick={handleClick}>
+          Write a Review
+        </Button>
       </div>
     </div>
   );
