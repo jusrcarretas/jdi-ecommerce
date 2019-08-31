a486c16f4c346b1f7b95014d686ac53861313099
Justin Carretas <jst.carretas@gmail.com>
2019-08-31 09:46:39 +0800

    Update shop/models.py - Added Category Model

--- django-jdi/shop/models.py
+++ django-jdi/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    # Details
+    name = models.CharField(max_length=100, verbose_name='Category Name', unique=True)
+    description = models.TextField(verbose_name='Category Description')
+
+    def __str__(self):
+        return self.name
