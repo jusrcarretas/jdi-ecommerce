39a46eb87ff2b4af3708cce9a614b1ca753c595c
Justin Carretas <jst.carretas@gmail.com>
2019-08-31 17:31:11 +0800

    Update src/components/Product/AddReview.js - Added Form For New Review

--- react-jdi/src/components/Product/AddReview.js
+++ react-jdi/src/components/Product/AddReview.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import { Button, Modal } from 'antd';
+import { Form, Input, Button, Modal } from 'antd';
 import './AddReview.css';
 import AddReviewHeader from './AddReviewHeader';
 
 class AddReview extends Component {
   state = {
-    rating: 5
+    rating: 5,
+    title: null,
+    comment: null
   }
 
   // Update rating state based on AddReview modal
   handleRatingChange = newRating => {
     this.setState({
       rating: newRating
+    });
+  }
+
+  handleTextChanged = e => {
+    this.setState({
+      [e.target.id]: e.target.value
     });
   }
 
@@ -23,22 +31,45 @@ class AddReview extends Component {
         onOk={this.props.handleOk}
         onCancel={this.props.handleCancel}
         footer={[
-          <Button 
-            key="back" 
+          <Button
+            key="back"
             onClick={this.props.handleCancel}>
             Cancel
           </Button>,
-          <Button 
-            key="ok" 
+          <Button
+            key="ok"
             type="primary"
-            onClick={this.props.handleOk}>
+            form="form-add-review"
+            htmlType="submit">
             Submit
           </Button>
         ]}>
         <AddReviewHeader
           productName={this.props.productName}
           rating={this.state.rating}
-          handleRatingChange={this.handleRatingChange}/>
+          handleRatingChange={this.handleRatingChange} />
+
+        <Form id="form-add-review" onSubmit={this.props.handleOk}>
+          <Form.Item label="Title">
+            <Input
+              type="text"
+              id="title"
+              value={this.state.title}
+              onChange={this.handleTextChanged}
+              placeholder="Summarize your review in a title"
+              required />
+          </Form.Item>
+          <Form.Item label="Review">
+            <Input.TextArea
+              value={this.state.comment}
+              id="comment"
+              placeholder="Tell us what you thought about our product"
+              onChange={this.handleTextChanged}
+              autosize={{ minRows: 3, maxRows: 5 }}
+              required
+            />
+          </Form.Item>
+        </Form>
       </Modal>
     );
   }
